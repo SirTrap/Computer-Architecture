b73c613b980a4f499b1d89f8096d637d505d42d1
SirTrap <ertugumsur@gmail.com>
2025-03-27 03:57:10 +0100

    Final Final

--- Project3/sine.sv
+++ Project3/sine.sv
@@ -1,29 +1,34 @@
 module sine(
     input  logic clk,
-    output logic _9b,
-    output logic _6a,
-    output logic _4a,
-    output logic _2a,
-    output logic _0a,
-    output logic _5a,
-    output logic _3b,
-    output logic _49a,
-    output logic _45a,
-    output logic _48b
+    output logic _9b, //D0
+    output logic _6a, //D1
+    output logic _4a, //D2
+    output logic _2a, //D3
+    output logic _0a, //D4
+    output logic _5a, //D5
+    output logic _3b, //D6
+    output logic _49a, //D7
+    output logic _45a, //D8
+    output logic _48b //D9
 );
 
+
     logic [8:0] timef = 0; 
+    //Memory Cell for 128 10-bit value storage
     logic [9:0] memory [0:127];
     logic [9:0] outputa;
 
+    //Copying the contents of the sine file into the memory cells
     initial begin 
         $readmemh("newsine.txt", memory);
     end
 
+    //Time logic for operating from 0 to 512
     always_ff @(posedge clk) begin
         timef <= (timef == 511) ? 0 : timef + 1;
     end
 
+    //Combinational Logic to find the sinewave region and compute the corresponding output
     always_comb begin
         if(timef < 128) begin
             outputa = memory[timef];
@@ -36,6 +41,7 @@ module sine(
         end
     end
    
+    //Assigning the output pin values to the 10 corresponding pins
     assign {_48b, _45a, _49a, _3b, _5a, _0a, _2a, _4a, _6a, _9b} = outputa[9:0];
 
     
